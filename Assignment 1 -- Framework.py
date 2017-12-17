7d58a8673927f581f4fd9712cd43a7ab7b3dcfa9
Eric Ortiz <e.ortiz.programmer@gmail.com>
2017-12-17 12:25:57 -0600

    Everything should work and it's all properly commented

--- Assignment 1 -- Framework.py
+++ Assignment 1 -- Framework.py
@@ -32,58 +32,147 @@ PyramidPointCloud = [apex, base1, base2, base3, base4]
 # Note that shortcuts like "apex = [0,50,100]" will not work as they build new
 # structures rather than modifying the existing Pyramid / PyramidPointCloud
 def resetPyramid():
+    # grab the global variables of Pyramid and its point cloud
+    global Pyramid
+    global PyramidPointCloud
+
+    # recreate the original points of the triangle
+    apex = [0, 50, 100]
+    base1 = [-50, -50, 50]
+    base2 = [50, -50, 50]
+    base3 = [50, -50, 150]
+    base4 = [-50, -50, 150]
+
+    # organize these points into their respective polygons
+    frontpoly = [apex, base1, base2]
+    rightpoly = [apex, base2, base3]
+    backpoly = [apex, base3, base4]
+    leftpoly = [apex, base4, base1]
+    bottompoly = [base4, base3, base2, base1]
+
+    # reset the pyramid and point cloud with the new polys
+    Pyramid = [bottompoly, frontpoly, rightpoly, backpoly, leftpoly]
+    PyramidPointCloud = [apex, base1, base2, base3, base4]
+
     print("resetPyramid stub executed.")
 
 # This function translates an object by some displacement.  The displacement is a 3D
 # vector so the amount of displacement in each dimension can vary.
 def translate(object, displacement):
+    # iterate through the points in the object
+    for i in range(len(object)):
+        point = object[i]
+        # for every point, add each component with that of the displacement component
+        for j in range(len(point)):
+            point[j] += displacement[j]
+
     print("translate stub executed.")
     
 # This function performs a simple uniform scale of an object assuming the object is
 # centered at the origin.  The scalefactor is a scalar.
 def scale(object,scalefactor):
+    # iterate through the points in the object
+    for i in range(len(object)):
+        point = object[i]
+        # for every point, multiply each component by the scalefactor
+        for j in range(len(point)):
+            point[j] *= scalefactor
+
     print("scale stub executed.")
 
 # This function performs a rotation of an object about the Z axis (from +X to +Y)
 # by 'degrees', assuming the object is centered at the origin.  The rotation is CCW
-# in a LHS when viewed from -Z [the location of the viewer in the standard postion]
+# in a LHS when viewed from -Z [the location of the viewer in the standard position]
 def rotateZ(object,degrees):
+    # first convert the degrees to radians
+    radians = math.radians(degrees)
+
+    # iterate through the polygons in the object and grab each point
+    for i in range(len(object)):
+        point = object[i]
+        # so that the points don't get manipulated during computation, assign the x and y values separately
+        x = point[0]
+        y = point[1]
+        # use the z-rotation function
+        point[0] = (x * math.cos(radians)) - (y * math.sin(radians))
+        point[1] = (x * math.sin(radians)) + (y * math.cos(radians))
+
     print("rotateZ stub executed.")
     
 # This function performs a rotation of an object about the Y axis (from +Z to +X)
 # by 'degrees', assuming the object is centered at the origin.  The rotation is CW
 # in a LHS when viewed from +Y looking toward the origin.
 def rotateY(object,degrees):
+    # first convert the degrees to radians
+    radians = math.radians(degrees)
+
+    # iterate through the polygons in the object and grab each point
+    for i in range(len(object)):
+        point = object[i]
+        # so that the points don't get manipulated during computation, assign the x and z values separately
+        x = point[0]
+        z = point[2]
+        # use the y-rotation function
+        point[0] = (x * math.cos(radians)) + (z * math.sin(radians))
+        point[2] = (-1 * (x * math.sin(radians))) + (z * math.cos(radians))
+
     print("rotateY stub executed.")
 
 # This function performs a rotation of an object about the X axis (from +Y to +Z)
 # by 'degrees', assuming the object is centered at the origin.  The rotation is CW
 # in a LHS when viewed from +X looking toward the origin.
 def rotateX(object,degrees):
+    # first convert the degrees to radians
+    radians = math.radians(degrees)
+
+    # iterate through the polygons in the object and grab each point
+    for i in range(len(object)):
+        point = object[i]
+        # so that the points don't get manipulated during computation, assign the y and z values separately
+        y = point[1]
+        z = point[2]
+        # use the x-rotation function
+        point[1] = (y * math.cos(radians)) - (z * math.sin(radians))
+        point[2] = (y * math.sin(radians)) + (z * math.cos(radians))
+
     print("rotateX stub executed.")
 
-# The function will draw an object by repeatedly callying drawPoly on each polygon in the object
+# The function will draw an object by repeatedly calling drawPoly on each polygon in the object
 def drawObject(object):
+    for i in range(len(object)):
+        drawPoly(object[i])
     print("drawObject stub executed.")
 
-# This function will draw a polygon by repeatedly callying drawLine on each pair of points
+# This function will draw a polygon by repeatedly calling drawLine on each pair of points
 # making up the object.  Remember to draw a line between the last point and the first.
 def drawPoly(poly):
+    for i in range(len(poly)):
+        drawLine(poly[i-1], poly[i])
     print("drawPoly stub executed.")
 
 # Project the 3D endpoints to 2D point using a perspective projection implemented in 'project'
 # Convert the projected endpoints to display coordinates via a call to 'convertToDisplayCoordinates'
 # draw the actual line using the built-in create_line method
 def drawLine(start,end):
-    #        w.create_line(startdisplay[0],startdisplay[1],enddisplay[0],enddisplay[1])
+    startproject = project(start)
+    endproject = project(end)
+
+    startdisplay = convertToDisplayCoordinates(startproject)
+    enddisplay = convertToDisplayCoordinates(endproject)
+
+    w.create_line(startdisplay[0],startdisplay[1],enddisplay[0],enddisplay[1])
     print("drawLine stub executed.")
 
 # This function converts from 3D to 2D (+ depth) using the perspective projection technique.  Note that it
 # will return a NEW list of points.  We will not want to keep around the projected points in our object as
 # they are only used in rendering
 def project(point):
-    ps = []
-    # some stuff happens
+    # grab the distance of the center of projection from the screen and use it to find the new points for ps
+    global d
+    xps = (d*point[0])/(d+point[2])
+    yps = (d*point[1])/(d+point[2])
+    zps = point[2]/(d+point[2])
+    ps = [xps, yps, zps]
     return ps
 
 # This function converts a 2D point to display coordinates in the tk system.  Note that it will return a
@@ -91,9 +180,10 @@ def project(point):
 # they are only used in rendering.
 def convertToDisplayCoordinates(point):
     displayXY = []
-    # some stuff happens
+    displayXY.append(point[0]+200)
+    displayXY.append(-point[1]+200)
+    displayXY.append(point[2])
     return displayXY
-    
 
 # **************************************************************************
 # Everything below this point implements the interface
